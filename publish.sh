07377bdd356c2733f83cda450cef184877e21d05
D. Doffy <cuongnsm@gmail.com>
2023-09-08 22:23:37 +0700

    add publish.sh and stress test page

---
+++ publish.sh
@@ -0,0 +1,2 @@
+./build.sh	
+scp -i ~/Documents/2022_2023/Documents/credentials/pem-nn-pp-credentials/githubrunner.pem bin/stresspage ubuntu@ec2-35-183-6-105.ca-central-1.compute.amazonaws.com:~/stresspage
