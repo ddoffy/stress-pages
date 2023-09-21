722cfb31f564b3317286ed2d901e463f49f7b023
D. Doffy <cuongnsm@gmail.com>
2023-09-21 11:20:37 +0700

    use the default context of lambda

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -27,34 +28,25 @@ var (
 
 func display() {
 	for {
-		fmt.Printf("Current Connections: %d - Current Requests: %d - The secons: %d\n", total, totalRequests, seconds)
 		select {
 		case <-currentConnections:
 			total++
+			fmt.Printf("Current Connections: %d - Current Requests: %d - The secons: %d\n", total, totalRequests, seconds)
 		case <-disconnections:
 			total--
+			fmt.Printf("Current Connections: %d - Current Requests: %d - The secons: %d\n", total, totalRequests, seconds)
 		case <-currentRequests:
 			totalRequests++
+			fmt.Printf("Current Connections: %d - Current Requests: %d - The secons: %d\n", total, totalRequests, seconds)
 		}
 	}
 }
 
-func increaseSeconds() {
-	for {
-		time.Sleep(1 * time.Second)
-		seconds++
-		if seconds == ttl {
-			os.Exit(0)
-		}
-	}
-}
-
-func main() {
+func handler() {
 	currentConnections = make(chan int, 10)
 	disconnections = make(chan int, 10)
 	currentRequests = make(chan int, 10)
 	go display()
-	go increaseSeconds()
 	numberOfTask := 10
 	url := "https://www.google.com"
 
@@ -82,9 +74,8 @@ func main() {
 	fmt.Println("Domain Name:", domainName)
 
 	var wg sync.WaitGroup
-
+	wg.Add(numberOfTask)
 	for i := 0; i < numberOfTask; i++ {
-		wg.Add(1)
 		go func(link string) {
 			defer func() {
 				disconnections <- 1
@@ -93,12 +84,14 @@ func main() {
 			currentConnections <- 1
 			collect(link)
 		}(url)
-		time.Sleep(time.Second)
+		time.Sleep(100 * time.Millisecond)
 	}
 
-	ttl = seconds + 300
-
 	wg.Wait()
+}
+
+func main() {
+	Lambda.Start(handler)
 }
 
 func collect(url string) {
@@ -112,13 +105,15 @@ func collect(url string) {
 			e.Request.Visit(nUrl)
 		} else if strings.HasPrefix(nUrl, "/") {
 			e.Request.Visit(host + nUrl)
-		} else if strings.HasPrefix(nUrl, "/") == false {
+		} else if !strings.HasPrefix(nUrl, "/") {
 			e.Request.Visit(url + "/" + nUrl)
 		}
 
 	})
+
 	c.OnRequest(func(r *colly.Request) {
 		currentRequests <- 1
+		log.Println("Visiting", r.URL.String())
 	})
 
 	c.Visit(url)
